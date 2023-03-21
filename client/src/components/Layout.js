0838dd7300643f8c73acca3aabb29a303a756eb9
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-03-21 20:49:13 +0100

    Ajout de la base frontend avec react

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     return (
         <div>
             <Header />
-            <main style={{ minHeight: '80vh' }}>
+            <main style={{ minHeight: 'calc(100vh - 213px)' }}>
                 {children}
             </main>
             <Footer />
