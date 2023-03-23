662843c869db27436ddac039b3cd9db2a3e03bb2
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-03-23 12:26:46 +0100

    Correction de beug sur la connexion

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -18,6 +18,8 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
+            //${process.env.REACT_APP_API}
+            //http://localhost:8080/api/v1/auth/login
             const res = await axios.post(`${process.env.REACT_APP_API}api/v1/auth/login`, { email, password });
             if (res && res.data.success) {
                 toast.success(res.data.message);
