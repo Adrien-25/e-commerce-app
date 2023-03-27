9aba8ceca73c33a635427d7766b405de83557c46
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-03-27 20:21:38 +0200

    Ajout des routes et des controllers de création/modification/récupération/suppression des catégories

---
+++ controllers/categoryController.js
@@ -0,0 +1,116 @@
+import categoryModel from "../models/categoryModel.js";
+import slugify from "slugify";
+
+// Create Category
+export const CreateCategoryController = async (req, res) => {
+    try {
+        const { name } = req.body;
+        if (!name) {
+            return res.Status(401).send({ message: 'Name is requiered' })
+        }
+        const existingCategory = await categoryModel.findOne({ name })
+        if (existingCategory) {
+            return res.status(200).send({
+                success: true,
+                message: 'Category Already Exist'
+            })
+        }
+        const category = await new categoryModel({ name, slug: slugify(name) }).save();
+        res.status(201).send({
+            succes: true,
+            message: 'new Category Created Successfully',
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error in Creating Category'
+        })
+    }
+};
+
+// Update cateogy
+export const UpdateCategoryController = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const { id } = req.params;
+
+        const category = await categoryModel.findByIdAndUpdate(
+            id,
+            { name, slug: slugify(name) },
+            { new: true }
+        );
+        res.status(201).send({
+            succes: true,
+            message: 'new Category Created Successfully',
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error in Updating Category'
+        })
+    }
+};
+
+// Get All Cateogy
+export const CategoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.find({});
+        res.status(201).send({
+            succes: true,
+            message: 'All Categories List',
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error while getting all categories'
+        })
+    }
+}
+// Single Cateogy
+export const SingleCategoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.findOne(
+            { slug: req.params.slug }
+        );
+        res.status(201).send({
+            success: true,
+            message: 'Get Single Category Successfully',
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error while Getting category'
+        })
+    }
+}
+// delete Cateogy
+export const DeleteCategoryController = async (req, res) => {
+    console.log(req.params);
+    try {
+        const { id } = req.params;
+        await categoryModel.findByIdAndDelete(id);
+        res.status(201).send({
+            success: true,
+            message: 'Category Deleted Successfully',
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: 'Error while Deleting Category'
+        })
+    }
+}
---
+++ routes/categoryRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { CategoryController, CreateCategoryController, DeleteCategoryController, SingleCategoryController, UpdateCategoryController } from '../controllers/categoryController.js';
+import { requireSignIn, isAdmin } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+// CREATE CATEGORY || POST 
+router.post(
+    '/create-category',
+    requireSignIn,
+    isAdmin,
+    CreateCategoryController
+);
+
+// UPDATE CATEGORY || PUT 
+router.put(
+    '/update-category/:id',
+    requireSignIn,
+    isAdmin,
+    UpdateCategoryController
+);
+
+// GETALL CATEGORY || GET 
+router.get(
+    '/get-category',
+    CategoryController
+);
+
+// SINGLE CATEGORY || GET 
+router.get(
+    '/single-category/:slug',
+    SingleCategoryController
+);
+
+// DELETE CATEGORY || DELETE 
+router.delete(
+    '/delete-category/:id',
+    requireSignIn,
+    isAdmin,
+    DeleteCategoryController
+);
+
+
+export default router;
