ea3b00b865cc299d91ddb0ca6abedd9bc9e25555
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-04-05 18:52:31 +0200

    Deployement settings

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ import authRoutes from './routes/authRoute.js';
 import cors from "cors";
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-// import Path from 'path';
-// import { fileURLToPath } from 'url';
+
 
 // configure env
 dotenv.config()
 
 //database config
 connectDB()
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
@@ -26,7 +22,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -34,12 +29,10 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api 
-// app.get('/', (req, res) => {
-//     res.send("<h1>Welcome to exommerce app</h1>");
-// })
-// app.use('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, './client/build/index.html'));
-// })
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to exommerce app</h1>");
+})
+
 
 //PORT
 const PORT = process.env.PORT || 8080;
